8f9ed975fd5a69df4b3e82974fbad503a67f474c
DaniloNogueira-Silva <dansilva730x@gmail.com>
2024-06-20 17:21:21 -0300

    fix: remove archives

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SpotsModule } from './spots/spots.module';
 
 @Module({
   imports: [EventsModule, PrismaModule, SpotsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
